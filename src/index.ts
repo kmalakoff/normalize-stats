9af81649383a1284543eeed2216aab6044c307e6
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 13:14:37 -0700

    Upgrade to mandatory typescript extensions

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 const kNsPerMsBigInt = typeof BigInt === 'undefined' ? 10 ** 6 : BigInt(10 ** 6);
 
 export default function normalizeStats(stats) {
-  if (typeof stats.dev !== 'bigint') {
+  if (typeof stats.dev === 'bigint') {
+    if (!stats.atimeNs) {
+      stats.atimeNs = stats.atimeMs * (kNsPerMsBigInt as bigint);
+      stats.mtimeNs = stats.mtimeMs * (kNsPerMsBigInt as bigint);
+      stats.ctimeNs = stats.ctimeMs * (kNsPerMsBigInt as bigint);
+      stats.birthtimeNs = stats.birthtimeMs * (kNsPerMsBigInt as bigint);
+    }
+  } else {
     if (!stats.atimeMs) {
       stats.atimeMs = stats.atime.valueOf() * 1000;
       stats.mtimeMs = stats.mtime.valueOf() * 1000;
       stats.ctimeMs = stats.ctime.valueOf() * 1000;
       stats.birthtimeMs = stats.birthtime ? stats.birthtime.valueOf() * 1000 : stats.mtimeMs;
-    }
-  } else {
-    if (!stats.atimeNs) {
-      stats.atimeNs = stats.atimeMs * kNsPerMsBigInt;
-      stats.mtimeNs = stats.mtimeMs * kNsPerMsBigInt;
-      stats.ctimeNs = stats.ctimeMs * kNsPerMsBigInt;
-      stats.birthtimeNs = stats.birthtimeMs * kNsPerMsBigInt;
     }
   }
   return stats;

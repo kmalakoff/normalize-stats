0eb3ff6372ebb18320f66bc00d850a96626d312b
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-13 22:20:12 -0700

    Upgrade modules

--- test/spec/normalize.test.ts
+++ test/spec/normalize.test.ts
@@ -31,7 +31,9 @@ describe('normalize', () => {
   });
   beforeEach((done) => {
     rimraf2(TEST_DIR, { disableGlob: true }, () => {
-      generate(TEST_DIR, STRUCTURE, done);
+      generate(TEST_DIR, STRUCTURE, (err) => {
+        done(err);
+      });
     });
   });
 
